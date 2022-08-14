fcd9205290757274c88458077558e6bb27f0ed45
Ankush Singh <ankushsahil45@gmail.com>
2022-08-14 20:07:46 +0530

    output loc update

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import "./Welcome.css";
 import Button from "../components/Button";
 import Input from "../components/Input";
-import Output from "../components/Output";
+import Output from "../components/Output/Output";
 
 const Welcome = () => {
   let [Fname, setFname] = useState("");
